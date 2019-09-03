01017b108012f210dbcc80abf887f08e94126eef
Kamil Nowak <kamilnowakx98@gmail.com>
2019-09-03 17:02:49 +0200

    scroll work

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.querySelector(".main");
+  let currentSectionIndex = 0;
+  let isFrezz = false;
   const sections = document.querySelectorAll(".section");
   document.addEventListener("mousewheel", e => {
-    console.log(e.wheelDelta);
+    const direction = e.wheelDelta < 0 ? 1 : -1;
+    const sectionsLenght = sections.length;
+    if (isFrezz) return;
+    isFrezz = true;
+    setTimeout(() => {
+      isFrezz = false;
+    }, 600);
+
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sectionsLenght - 1;
+      if (!isLastSection) {
+        currentSectionIndex += direction;
+      }
+    } else if (direction === -1) {
+      const isFirstSection = currentSectionIndex === 0;
+      if (!isFirstSection) {
+        currentSectionIndex += direction;
+      }
+    }
+    sections[currentSectionIndex].scrollIntoView({
+      block: "start",
+      behavior: "smooth"
+    });
   });
 });
