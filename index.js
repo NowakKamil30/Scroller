46dd735c5fddc90cd0abae7f2f4267965f9a6413
Kamil Nowak <kamilnowakx98@gmail.com>
2019-09-03 19:29:14 +0200

    add support for keyboard and touch screen

--- index.js
+++ index.js
@@ -2,4 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const scroller = new Scroller(".main");
 
   document.addEventListener("mousewheel", scroller.listemStroll);
+  document.addEventListener("swipeUp", () => scroller.scroll(1));
+  document.addEventListener("swipeDown", () => scroller.scroll(-1));
+  document.addEventListener("keydown", e => {
+    switch (e.keyCode) {
+      case 38:
+        return scroller.scroll(-1);
+        break;
+      case 40:
+        return scroller.scroll(1);
+        break;
+      default:
+        return;
+        break;
+    }
+  });
 });
